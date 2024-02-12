c57bd17a03a8e5e356c2cf59758750fe23c66f5e
jatin2198 <jatinarya2198@gmail.com>
2024-02-12 13:05:25 +0530

    ZipWith and Zip
    ZipWith and Zip

--- reactive-java-app/src/test/java/com/reactive/app/ReactiveJavaAppApplicationTests.java
+++ reactive-java-app/src/test/java/com/reactive/app/ReactiveJavaAppApplicationTests.java
@@ -7,6 +7,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.util.concurrent.MonoToListenableFutureAdapter;
 
 import reactor.core.publisher.Mono;
+import reactor.util.function.Tuple2;
 
 @SpringBootTest
 class ReactiveJavaAppApplicationTests {
@@ -17,34 +18,47 @@ class ReactiveJavaAppApplicationTests {
 
 	@Test
 	void workWithMono() {
-		
-		//mono--publisher whixh will return 0 or 1
-		//created mono
-		Mono<Object> monoErro=	Mono
-				.error(new RuntimeException("Throwing an Error...."))
-				.log();
-		Mono<Object> mono= Mono
-				.just("learning reactive java")
-				.log().then(monoErro);
-		
-		/* Consumed Mono
-		 * mono.subscribe(new Consumer<String>() {
+
+		/*
+		 * //mono--publisher whixh will return 0 or 1 //created mono Mono<Object>
+		 * monoErro= Mono .error(new RuntimeException("Throwing an Error....")) .log();
+		 * Mono<Object> mono= Mono .just("learning reactive java")
+		 * .log().then(monoErro);
+		 * 
+		 * Consumed Mono mono.subscribe(new Consumer<String>() {
 		 * 
 		 * 
 		 * 
 		 * @Override public void accept(String t) { // TODO Auto-generated method stub
 		 * System.out.println("Consumed data= "+t); } });
+		 * 
+		 * 
+		 * //doing same with lambda
+		 * 
+		 * mono.subscribe(data->System.out.println("Consumed data= "+data));
 		 */
-		
-		//doing same with lambda 
-		
-	mono.subscribe(data->System.out.println("Consumed data= "+data));
-		//mono.subscribe(System.out::println);
-	
-	
+		// mono.subscribe(System.out::println);
 
-	
-	//monoErro.subscribe(System.out::println);
+		// monoErro.subscribe(System.out::println);
+		
+		Mono<String> m1=Mono.just("Jatin hi Shivam h");
+		Mono<String> m2=Mono.just("Shivam hi Jatin h");
+		
+		Mono<Tuple2<String, String>> combined=Mono.zip(m1, m2).log();
+		
+	//	combined.subscribe(System.out::println);
+		
+		combined.subscribe(data->{
+			System.out.println("Consumed from m1= "+data.getT1());
+			System.out.println("Consumed from m2= "+data.getT2());
+		});
+		
+		Mono<Tuple2<String, String>> combWithZip=m1.zipWith(m2);
+		combWithZip.subscribe(data->{
+			System.out.println("Consumed from m1= "+data.getT1());
+			System.out.println("Consumed from m2= "+data.getT2());
+		});
+		
 	}
 
 }
