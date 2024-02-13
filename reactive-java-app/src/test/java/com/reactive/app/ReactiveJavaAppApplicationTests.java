458e8b90209f9b96f2d764e24ade6013fca7daa8
jatin2198 <jatinarya2198@gmail.com>
2024-02-13 11:36:29 +0530

    map,flattmap,flatmapMany
    map,flattmap,flatmapMany

--- reactive-java-app/src/test/java/com/reactive/app/ReactiveJavaAppApplicationTests.java
+++ reactive-java-app/src/test/java/com/reactive/app/ReactiveJavaAppApplicationTests.java
@@ -1,11 +1,13 @@
 package com.reactive.app;
 
+import java.util.Arrays;
 import java.util.function.Consumer;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.util.concurrent.MonoToListenableFutureAdapter;
 
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.util.function.Tuple2;
 
@@ -44,21 +46,29 @@ class ReactiveJavaAppApplicationTests {
 		Mono<String> m1=Mono.just("Jatin hi Shivam h");
 		Mono<String> m2=Mono.just("Shivam hi Jatin h");
 		
-		Mono<Tuple2<String, String>> combined=Mono.zip(m1, m2).log();
+		//Mono<Tuple2<String, String>> combined=Mono.zip(m1, m2).log();
 		
 	//	combined.subscribe(System.out::println);
 		
-		combined.subscribe(data->{
-			System.out.println("Consumed from m1= "+data.getT1());
-			System.out.println("Consumed from m2= "+data.getT2());
-		});
+	/*
+	 * combined.subscribe(data->{
+	 * System.out.println("Consumed from m1= "+data.getT1());
+	 * System.out.println("Consumed from m2= "+data.getT2()); });
+	 * 
+	 * Mono<Tuple2<String, String>> combWithZip=m1.zipWith(m2);
+	 * combWithZip.subscribe(data->{
+	 * System.out.println("Consumed from m1= "+data.getT1());
+	 * System.out.println("Consumed from m2= "+data.getT2()); });
+	 */
+			m1.map(item->item.toUpperCase()).subscribe(System.out::println);
+			
+			m1.flatMap(valueM1->Mono.just(valueM1.split(" "))).subscribe(item->{
+				Arrays.asList(item).forEach(values->System.out.println(values));
+			});
+			
+		Flux<String> f1=m1.flatMapMany(valueM1->Flux.just(valueM1.split(" "))).log();
 		
-		Mono<Tuple2<String, String>> combWithZip=m1.zipWith(m2);
-		combWithZip.subscribe(data->{
-			System.out.println("Consumed from m1= "+data.getT1());
-			System.out.println("Consumed from m2= "+data.getT2());
-		});
-		
+		f1.subscribe(data->System.out.println("data from Flux----->"+data));
 	}
 
 }
